bdbd214dcd44176e7f5408de38c9649bae91cae0
alexthebrown <99758041+alexthebrown@users.noreply.github.com>
2025-06-13 21:32:19 -0500

    for pi

--- run_nomad.py
+++ run_nomad.py
@@ -1,11 +1,12 @@
 from src.audio_player import AudioPlayer
 from src.matcher import match_trigger
 from src.recognizer_mac import MacSpeechRecognizer
+from src.recognizer import SpeechRecognizer
 from src.responder import Responder
 from src.speaker import Speaker
 from web.server import run_server, get_control_flags, log_event
 
-recognizer = MacSpeechRecognizer()
+recognizer = SpeechRecognizer()
 responder = Responder()
 speaker = Speaker()
 audioPlayer = AudioPlayer()
