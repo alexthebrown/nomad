b07164765e8f2f1e61a96d49b2e6c1ac0ab5bd61
alexthebrown <99758041+alexthebrown@users.noreply.github.com>
2025-06-19 01:03:23 -0500

    pair down

--- src/speaker.py
+++ src/speaker.py
@@ -17,19 +17,19 @@ class Speaker:
                 break
 
     def speak(self, text):
-        if self.led_controller:
-            led_thread = threading.Thread(target=self._pulse_leds)
-            led_thread.start()
+        # if self.led_controller:
+        #     led_thread = threading.Thread(target=self._pulse_leds)
+        #     led_thread.start()
 
         self.engine.say(text)
         self.engine.runAndWait()
 
-        if self.led_controller:
-            self.led_controller.off()
+        # if self.led_controller:
+        #     self.led_controller.off()
 
-    def _pulse_leds(self):
-        # Simulated LED pulsing during speech
-        while self.engine._inLoop:
-            for level in [0.3, 0.6, 1.0, 0.6]:
-                self.led_controller.flash(level, duration=0.1)
-            time.sleep(0.05)
+    # def _pulse_leds(self):
+    #     # Simulated LED pulsing during speech
+    #     while self.engine._inLoop:
+    #         for level in [0.3, 0.6, 1.0, 0.6]:
+    #             self.led_controller.flash(level, duration=0.1)
+    #         time.sleep(0.05)
